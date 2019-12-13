a1bd598733f1f4e4fdc1311f512d3c4809c47da0
JG <noreply@jg.com>
2019-12-13 13:17:04 -0800

    added ODMB AJAX call to script.js changed HTML for JS support

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert("I'm here");
+//function to parse the moviename and replace spaces with +
+
+//Declare Global variables
+var myMovie= "Star+Wars";
+
+var myODMBMovieURL = "Star+Wars";
+
+SearchOMDB();
+
+function SearchOMDB(){
+ $.ajax({
+    // url: "https://www.omdbapi.com/?t=the+dark+night&y=&plot=short&apikey=trilogy",
+    url: "https://www.omdbapi.com/?t="+myMovie+"&y=&plot=short&apikey=trilogy",
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
+
+}
