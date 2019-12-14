e42556360f5635c3f4d804b3a792cd0be2389271
JG <noreply@jg.com>
2019-12-14 12:20:41 -0800

    added global variables and functions for getting OMDB values

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
+//Click Button Event 
+
+
+//**********************************************OMBDB Functions Section ****************************************/
 //function to parse the moviename and replace spaces with +
 
-//Declare Global variables
-var myMovie= "Star+Wars";
 
-var myODMBMovieURL = "Star+Wars";
 
-SearchOMDB();
+//***********************Declare Global variables for OMDB****************************************************
+var myMovie= "Star Wars";
+var OMDBresponse;
+var omTitle;
+var omPosterUrl;
 
-function SearchOMDB(){
+
+//
+myMovie= myMovie.replace(" ","+");  //create string to insert into url request
+console.log("OMDB:myMovie = " + myMovie);
+
+
+OMDBresponse = getOMDB();
+
+function getOMDB(){
  $.ajax({
     // url: "https://www.omdbapi.com/?t=the+dark+night&y=&plot=short&apikey=trilogy",
     url: "https://www.omdbapi.com/?t="+myMovie+"&y=&plot=short&apikey=trilogy",
     method: "GET"
   }).then(function(response) {
     console.log(response);
+    return response;
   });
+  
+}
+
+
+function getOMDBValues(){
+    omTitle = OMDBresponse.Title;
+   console.log("omTitle = " + omTitle);
+    omPosterUrl= OMDBresponse.Poster;
+   console.log("OmPosterURl="+ omPosterUrl );
+    $("img").attr("url",omPosterUrl);
+
+
+
 
 }
